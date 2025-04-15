f685f68f33c5bc818a62b8a37a0e2a7a3bed6c1e
atoleon <atoleon@gmail.com>
2025-04-15 09:31:09 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const { Resend } = require("resend");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const resend = new Resend("re_XFCohrX2_22FajmGAwF4xBfcUfVtAUP9G");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.post("/contactform", async (req, res) => {
+  const { name, email, subject, message } = req.body;
+  console.log(name, email, subject, message);
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: "atoleon@gmail.com",
+    subject: "Contacto RDC",
+    html: `<p>${name}</p><p>${email}</p><p>${subject}</p><p>${message}</p>`,
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
+
+  console.log({ data });
+});
+
+app.listen(3000);
+console.log("listening on port 3000");
