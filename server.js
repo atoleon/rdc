98cc71d383ee65dcb853d0e5c773863787520618
atoleon <atoleon@gmail.com>
2025-04-15 10:02:36 +0100

    setup port for dev and prod

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const resend = new Resend("re_XFCohrX2_22FajmGAwF4xBfcUfVtAUP9G");
 
 app.use(bodyParser.urlencoded({ extended: true }));
